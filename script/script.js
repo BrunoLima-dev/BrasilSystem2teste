1f57586c01498959c99b97656e46deff135f74c8
BrunoLima-dev <brunoprograme04@gmail.com>
2022-05-05 22:31:53 -0300

    pegando dados no formulaio

--- script/script.js
+++ script/script.js
@@ -1,5 +1,13 @@
 var btnSolicitar = document.getElementById("btnSolicitar");
 
-btnSolicitar.addEventListener("click", function () {
-    console.log("fui clicado");
+btnSolicitar.addEventListener("click", function (event) {
+    event.preventDefault();
+    
+    var frmSolicitar = document.getElementById("frmSolicitar");
+    console.log(frmSolicitar.autor.value);
+    console.log(frmSolicitar.titulo.value);
+    console.log(frmSolicitar.issn.value);
+    console.log(frmSolicitar.editora.value);
+    console.log(frmSolicitar.edicao.value);
+    console.log(frmSolicitar.ano.value);
 })
