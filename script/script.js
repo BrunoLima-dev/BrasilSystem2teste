81e56a90a799feab45b8df35b9c80335f8d938b4
BrunoLima-dev <brunoprograme04@gmail.com>
2022-05-08 01:00:43 -0300

    upgrade

--- script/script.js
+++ script/script.js
@@ -1,24 +1,30 @@
 var btnSolicitar = document.getElementById("btnSolicitar");
+var btnBloquear = document.getElementById("btnSolicitar");
+  
 
 btnSolicitar.addEventListener("click", function (event) {
-    event.preventDefault();
+ 
+
+  var frmSolicitar = document.getElementById("frmSolicitar");
+  const select = document.getElementById("select");
+
+  btnBloquear.addEventListener("click", (event)); {
+    var ano = document.getElementById("ano"); 
+       
+
+    if (frmSolicitar.ano.value < 1900 || frmSolicitar.ano.value > 2019 ) {
+        ano.setCustomValidity("Ano inválido");
+        ano.reportValidity();
+        alert("Digite Ano entere entre 1900 e 2019");
+        ano.block();        
+      }
+    };
+        
     
-    var frmSolicitar = document.getElementById("frmSolicitar"); 
-    const select = document.getElementById("select");  
+    const indice = select.selectedIndex;
+    const text = select.options[indice].text;  
 
-   
-
-    const indice = select.selectedIndex;    
-    const text = select.options[indice].text;
-    console.log(text);
-    console.log(frmSolicitar.titulo.value);
-    console.log(frmSolicitar.autor.value);
-    console.log(frmSolicitar.edicao.value);
-    console.log(frmSolicitar.issn.value);
-    console.log(frmSolicitar.ano.value);
-    console.log(frmSolicitar.editora.value);
-
-   //Cria o elemento tr
+    //Cria o elemento tr
     var linhaTabela = document.createElement("tr");
 
     //Cria os elementos td
@@ -32,17 +38,14 @@ btnSolicitar.addEventListener("click", function (event) {
 
     //Adicionar o conteudo de cada celular
     colunaVinculo.textContent = text;
-    colunaTitulo.textContent = frmSolicitar.titulo.value;   
+    colunaTitulo.textContent = frmSolicitar.titulo.value;
     colunaAutor.textContent = frmSolicitar.autor.value;
     colunaEdicao.textContent = frmSolicitar.edicao.value;
     colunaISSN.textContent = frmSolicitar.issn.value;
     colunaAno.textContent = frmSolicitar.ano.value;
     colunaEditora.textContent = frmSolicitar.editora.value;
 
-
-    
-
-   //Obtendo referencia da tabela solicitar
+    //Obtendo referencia da tabela solicitar
     var tabelaSolicita = document.getElementById("table").querySelector("tbody");
 
     //inserindo todas as celulas dentro da linha
@@ -54,6 +57,52 @@ btnSolicitar.addEventListener("click", function (event) {
     tabelaSolicita.appendChild(colunaAno);
     tabelaSolicita.appendChild(colunaEditora);
 
-
     tabelaSolicita.appendChild(linhaTabela);
+});
+
+  // Bloqueia os inputs do formulario
+  function block() {
+    document.querySelector("select").disabled = true;
+    document.getElementById("autor").disabled = true;
+    document.getElementById("titulo").disabled = true;
+    document.getElementById("issn").disabled = true;
+    document.getElementById("editora").disabled = true;
+    document.getElementById("edicao").disabled = true;
+    document.getElementById("ano").disabled = true;
+  }
+
+  //Inicio Contador regressivo
+function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+
+  setInterval(function () {
+
+    minutes = parseInt(timer / 60, 10)
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      timer = duration;
+    }else if (timer == 0) {
+      block();
+      alert("Tempo esgotado");
+    }
+    
+  }, 1000);
+}
+
+
+  window.onload = function () {
+    var duration = 3600 * 1; // Conversão para segundos
+    var display = document.querySelector('#time');
+    startTimer(duration, display);
+  }
+ 
+
+  
+
 
